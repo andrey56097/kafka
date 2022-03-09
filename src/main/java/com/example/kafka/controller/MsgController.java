b886b36ea7e8034b08e0a517bdd292f305c23a58
DESKTOP-46TCRJJ\Andrew <46>
2022-03-09 13:40:21 +0200

    add lombok sm refactoring

--- src/main/java/com/example/kafka/controller/MsgController.java
+++ src/main/java/com/example/kafka/controller/MsgController.java
@@ -2,6 +2,8 @@ package com.example.kafka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,8 @@ public class MsgController {
 
     @PostMapping
     public void sendOrder(String msgId, String msg){
-        kafkaTemplate.send("msg", msgId, msg);
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("msg", msgId, msg);
+        future.addCallback(System.out::println, System.err::println);
+        kafkaTemplate.flush();
     }
 }
